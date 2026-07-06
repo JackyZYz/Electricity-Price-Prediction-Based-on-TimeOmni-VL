722a8c04f3da81a56156d161901467fdddb22642
JackyZYz <jackyzhouyuzhe@outlook.com>
2026-07-06 09:08:52 +0800

    Wire optimizer setup into train CLI and add gradient clipping for Janus adapter

--- timeomni_vl/cli/train.py
+++ timeomni_vl/cli/train.py
@@ -5,11 +5,14 @@ from torch.utils.data import DataLoader
 
 from timeomni_vl.config import ConfigManager
 from timeomni_vl.data.pipeline import build_data_pipeline
+from timeomni_vl.logger import get_logger
 from timeomni_vl.models import build_adapter
 from timeomni_vl.training.collator import TSUMMCollator
 from timeomni_vl.training.dataset import TSUMMDatasetBuilder
 from timeomni_vl.training.trainer import TSUMMTrainer
 from timeomni_vl.utils.io import ensure_dir
+
+logger = get_logger(__name__)
 
 
 def main():
@@ -25,6 +28,19 @@ def main():
 
     adapter = build_adapter(model_cfg.backbone)
     adapter.load(model_cfg.model_path, model_cfg.device)
+
+    # Setup optimizer/LoRA for real backbones (Janus/Bagel)
+    if hasattr(adapter, "setup_optimizer"):
+        adapter.setup_optimizer(
+            lr=training_cfg.lr,
+            lora_enabled=training_cfg.lora_enabled,
+            lora_rank=training_cfg.lora_rank,
+            lora_alpha=training_cfg.lora_alpha,
+            lora_target_modules=training_cfg.lora_target_modules,
+            max_grad_norm=training_cfg.max_grad_norm,
+        )
+        trainable = adapter.get_trainable_params()
+        logger.info(f"Trainable params: {trainable}")
 
     data_pipeline = build_data_pipeline(data_cfg)
     samples = data_pipeline.run()
